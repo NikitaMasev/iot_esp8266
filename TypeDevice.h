bcbf944ee223fc6a07c1418ff6df8bd1896683d6
Nikita Masev <n.masev@bastion-tech.ru>
2022-12-15 00:07:02 +0300

    added type control enum, convert function for type device enum

--- TypeDevice.h
+++ TypeDevice.h
@@ -1,25 +1,49 @@
-typedef enum { ups,
-               lamp,
-               rgba,
-               unknown,
-} TypeDevice;
+#include "WString.h"
+
+enum TypeDevice {
+  ups,
+  lamp,
+  rgba,
+  rgbaEffects,
+  tempSensor,
+  unknown,
+};
+
+enum TypeControl {
+  powerOn_c,
+  powerOff_c,
+  rgba_c,
+  rgbaEffects_c,
+  changeName_c,
+  register_c,
+};
 
 const static struct {
   TypeDevice val;
   const char *str;
-} conversion[] = {
+} typeDeviceToStr[] = {
   { ups, "ups" },
   { lamp, "lamp" },
   { rgba, "rgba" },
+  { rgbaEffects, "rgbaAddress" },
+  { tempSensor, "tempSensor" },
   { unknown, "unknown" },
 };
 
-TypeDevice str2enum(const char *str) {
-  int j;
-  for (j = 0; j < sizeof(conversion) / sizeof(conversion[0]); ++j) {
-    if (!strcmp(str, conversion[j].str)) {
-      return conversion[j].val;
+TypeDevice stringToTypeDevice(const char *str) {
+  for (int j = 0; j < sizeof(typeDeviceToStr) / sizeof(typeDeviceToStr[0]); ++j) {
+    if (strcmp(str, typeDeviceToStr[j].str)) {
+      return typeDeviceToStr[j].val;
     }
   }
   return unknown;
+}
+
+String typeDeviceToString(TypeDevice typeDevice) {
+  for (int j = 0; j < sizeof(typeDeviceToStr) / sizeof(typeDeviceToStr[0]); ++j) {
+    if (typeDevice == typeDeviceToStr[j].val) {
+      return typeDeviceToStr[j].str;
+    }
+  }
+  return typeDeviceToStr[unknown].str;
 }
