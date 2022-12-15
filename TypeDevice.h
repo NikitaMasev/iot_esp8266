96bf77ae23353fe88c849a7aa564aa59eeab8da5
Nikita Masev <n.masev@bastion-tech.ru>
2022-12-15 11:46:29 +0300

    update type device header file, conversation to allowing type control by type device

--- TypeDevice.h
+++ TypeDevice.h
@@ -1,10 +1,12 @@
 #include "WString.h"
 
+#define SIZE_TYPE_CONTROL_MAP 6
+
 enum TypeDevice {
   ups,
   lamp,
   rgba,
-  rgbaEffects,
+  rgbaAddress,
   tempSensor,
   unknown,
 };
@@ -16,6 +18,7 @@ enum TypeControl {
   rgbaEffects_c,
   changeName_c,
   register_c,
+  unknown_c,
 };
 
 const static struct {
@@ -25,9 +28,34 @@ const static struct {
   { ups, "ups" },
   { lamp, "lamp" },
   { rgba, "rgba" },
-  { rgbaEffects, "rgbaAddress" },
+  { rgbaAddress, "rgbaAddress" },
   { tempSensor, "tempSensor" },
   { unknown, "unknown" },
+};
+
+const static struct {
+  TypeControl val;
+  const char *str;
+} typeControlToStr[] = {
+  { powerOn_c, "powerOn" },
+  { powerOff_c, "powerOff" },
+  { rgba_c, "rgba" },
+  { rgbaEffects_c, "rgbaEffects" },
+  { changeName_c, "changeName" },
+  { register_c, "register" },
+  { unknown_c, "unknown" },
+};
+
+const static struct {
+  TypeDevice typeDevice;
+  TypeControl typeControl[SIZE_TYPE_CONTROL_MAP];
+} typeDeviceToTypeControl[] = {
+  { ups, { changeName_c, register_c } },
+  { lamp, { powerOn_c, powerOff_c, changeName_c, register_c } },
+  { rgba, { powerOn_c, powerOff_c, changeName_c, register_c, rgba_c } },
+  { rgbaAddress, { powerOn_c, powerOff_c, changeName_c, register_c, rgbaEffects_c, rgba_c } },
+  { tempSensor, { changeName_c, register_c } },
+  { unknown, {} },
 };
 
 TypeDevice stringToTypeDevice(const char *str) {
@@ -46,4 +74,36 @@ String typeDeviceToString(TypeDevice typeDevice) {
     }
   }
   return typeDeviceToStr[unknown].str;
+}
+
+TypeControl stringToTypeControl(const char *str) {
+  for (int j = 0; j < sizeof(typeControlToStr) / sizeof(typeControlToStr[0]); ++j) {
+    if (strcmp(str, typeControlToStr[j].str)) {
+      return typeControlToStr[j].val;
+    }
+  }
+  return unknown_c;
+}
+
+String typeControlToString(TypeControl typeControl) {
+  for (int j = 0; j < sizeof(typeControlToStr) / sizeof(typeControlToStr[0]); ++j) {
+    if (typeControl == typeControlToStr[j].val) {
+      return typeControlToStr[j].str;
+    }
+  }
+  return typeControlToStr[unknown_c].str;
+}
+
+bool allowedTypeControl(TypeDevice typeDevice, TypeControl typeControl) {
+  for (int i = 0; i < sizeof(typeDeviceToTypeControl) / sizeof(typeDeviceToTypeControl[0]); i++) {
+    if (typeDeviceToTypeControl[i].typeDevice == typeDevice) {
+      for (int j = 0; j < sizeof(typeDeviceToTypeControl[i].typeControl) / sizeof(typeDeviceToTypeControl[i].typeControl[0]); j++) {
+        if (typeDeviceToTypeControl[i].typeControl[j] == typeControl) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+  return false;
 }
