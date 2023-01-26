37cfe138baf469421098d035f653c298434a901d
Nikita Masev <n.masev@bastion-tech.ru>
2023-01-26 12:17:50 +0300

    changed timings in tasker

--- Config.h
+++ Config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-//#define TYPE_DEVICE_UPS +
+#define TYPE_DEVICE_UPS +
 //#define TYPE_DEVICE_LAMP +
-//#define TYPE_DEVICE_RGBA
-#define TYPE_DEVICE_RGBA_ADDRESS
+//#define TYPE_DEVICE_RGBA + 
+//#define TYPE_DEVICE_RGBA_ADDRESS + 
 //#define TYPE_DEVICE_TEMP_SENSOR
--- Tasker.h
+++ Tasker.h
@@ -33,16 +33,20 @@ GyverOS<2> tasker;
 #endif
 
 void setupTasker() {
-  tasker.attach(0, networkLoop, 250);  //50 OK 100??? 250 OK
-  tasker.attach(1, loopDataPusher, 350);
 #if defined(TYPE_DEVICE_UPS)
+  tasker.attach(0, networkLoop, 100);  //50 OK 100??? 250 OK
+  tasker.attach(1, loopDataPusher, 150);
   tasker.attach(2, loopCooler, 5000);
-  tasker.attach(3, loopTempDetector, 750);
+  tasker.attach(3, loopTempDetector, 400);
   tasker.attach(4, loopVoltCur, 200);
 #elif defined(TYPE_DEVICE_RGBA_ADDRESS)
+  tasker.attach(0, networkLoop, 250);  //50 OK 100??? 250 OK
+  tasker.attach(1, loopDataPusher, 350);
   tasker.attach(2, loopLedAddress, 10);  //10 OK 20??
 #elif defined(TYPE_DEVICE_TEMP_SENSOR)
-  tasker.attach(2, loopTempDetector, 750);
+  tasker.attach(0, networkLoop, 100);  //50 OK 100??? 250 OK
+  tasker.attach(1, loopDataPusher, 150);
+  tasker.attach(2, loopTempDetector, 400);
 #endif
 }
 
